9b2ff8c3044ab140cab5da059cdc1ff76ccd1160
atylermoses123 <atylermoses123@gmail.com>
2025-04-21 14:41:16 -0400

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+/*
+Student Name: Tyler Moses
+File Name: script.js
+Date: 3/24/2025
+*/
+
+// Hamburger menu function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    var logo = document.getElementById("ffc-logo");
+    if (menu.style.display === "block" && logo.style.display === "none") {
+        menu.style.display = "none";
+        logo.style.display = "block";
+    } else {
+        menu.style.display = "block";
+        logo.style.display = "none";
+    }
+}
